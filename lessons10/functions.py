2808ed42b37745b84420b29f523f8afd7fe093a8
oksana.safronova <oksana.aeshina666@gmail.com>
2022-05-30 16:48:31 +0300

    Add img, add docstrings

--- lessons10/functions.py
+++ lessons10/functions.py
@@ -3,6 +3,7 @@ f = open("candidates.json")
 file = json.load(f)
 
 def get_list_candidates():
+    """Получает список кандидатов"""
     list_candidates = ''
     for dict in file:
         list_candidates += f'{dict["name"]}\n'
@@ -12,17 +13,20 @@ def get_list_candidates():
     return '<pre>' + list_candidates + '</pre>'
 
 def get_some_candidate_by_id(pk):
+    """Получает кандидата по номеру id"""
     list_candidates = ''
     for dict in file:
         if pk == dict["id"]:
-            list_candidates += f'{dict["picture"]}\n'
+            list_candidates += f'<img src=\"{dict["picture"]}\">\n'
             list_candidates += f'{dict["name"]}\n'
             list_candidates += f'{dict["position"]}\n'
             list_candidates += f'{dict["skills"]}\n'
+            list_candidates += f'\n'
 
     return '<pre>' + list_candidates + '</pre>'
 
 def get_list_candidates_with_skill(skill):
+    """Получает список кандидатов с определенным скиллом"""
     list_candidates = ''
     for dict in file:
         list_skills = dict["skills"].lower().strip().split(', ')
